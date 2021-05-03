274de2c9d84a1f523816cfb63434469fd407f89c
Marta Brüning <marta@icloud.com>
2021-05-03 20:21:50 +0200

    workin on boost::bind() in contrroller.cpp

--- src/controller.cpp
+++ src/controller.cpp
@@ -28,7 +28,7 @@ class Controller
             img_subscriber.subscribe(node, "processor/image", 1);
             int_subscriber.subscribe(node, "camera/integer", 1);  
             
-            sync.registerCallback(callback); // boost::bind() allows to pass arguments to a callback. E.g. a map<int, string> 
+            sync.registerCallback(boost::bind(&Controller::callback, this, _1, _2)); // boost::bind() allows to pass arguments to a callback. E.g. a map<int, string> 
         }
 
         /* Sends the image as servide message to the neural network node.
@@ -76,6 +76,7 @@ class Controller
                     //std::map<sensor_msgs::ImageConstPtr, imagineer::Number>& storage,
                     //imagineer::ImageAck ack_service,
                     //ros::ServiceClient service_client)
+                    )
         {
             try
             {
