fd18e0764343e442a9c2d4b500f00f1d4ad6d1db
Marta Brüning <marta@icloud.com>
2021-05-03 19:03:30 +0200

    deleted sensor_header line

--- src/camera.cpp
+++ src/camera.cpp
@@ -19,16 +19,18 @@ int main(int argc, char** argv)
     image_transport::Publisher img_publisher = transport.advertise("camera/image", 1);
     ros::Publisher int_publisher = node.advertise<std_msgs::Int32>("camera/integer", 1);
     cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR);
+    std_msgs::Header header;
+    header.stamp = ros::Time::now();
     sensor_msgs::ImagePtr img_message = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
     std_msgs::Int32 int_message;
-    std_msgs::Header std_header;
+    
     ros::Rate loop_rate(1);
     int_message.data = 2;
 
     // as long as the node is running send the image and integer messages.
     while (node.ok()) 
     {
-        std_header.stamp = ros::Time::now();
+    
         img_publisher.publish(img_message);
         int_publisher.publish(int_message);
         ros::spinOnce();
