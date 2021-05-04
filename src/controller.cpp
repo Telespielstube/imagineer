7bc6e40fbd00dbd6c6536ed778cdfc09091963c6
Marta Brüning <marta@icloud.com>
2021-05-04 14:15:12 +0200

    Removed boost::bind

--- src/controller.cpp
+++ src/controller.cpp
@@ -49,7 +49,7 @@ class Controller
             img_subscriber.subscribe(node, "processor/image", 1);
             int_subscriber.subscribe(node, "camera/integer", 1); 
            // imagineer::ImageAck ack_service; 
-            sync.registerCallback(boost::bind(&Controller::callback, this, _1, ack_service)); // boost::bind() allows to pass arguments to a callback. E.g. a map<int, string> 
+            sync.registerCallback(&Controller::callback)); // boost::bind() allows to pass arguments to a callback. E.g. a map<int, string> 
         }
 
         /* Sends the image as servide message to the neural network node.
@@ -87,7 +87,7 @@ class Controller
         * @int_message
         * @map          map<> data structure to save the messages from the topics as key value pairs.
         */
-        void callback(const sensor_msgs::ImageConstPtr& image, const imagineer::Number& digit, imagineer::ImageAck& ack_service)
+        void callback(const sensor_msgs::ImageConstPtr& image, const imagineer::Number& digit)
         {
             try
             {
