df948eaa9bc67dafa49434176e277db275560114
Marta Brüning <marta@icloud.com>
2021-05-05 10:50:33 +0200

    working on registerCallback()

--- src/controller.cpp
+++ src/controller.cpp
@@ -47,14 +47,11 @@ class Controller
         message_filters::Subscriber<imagineer::Number> int_subscriber;
         message_filters::TimeSynchronizer<sensor_msgs::Image, imagineer::Number> sync;
         
-        Controller() = delete;
-        
         Controller() : sync(img_subscriber, int_subscriber, 10)
         {
             service_client = node.serviceClient<imagineer::ImageAck>("ImageAck");
             img_subscriber.subscribe(node, "processor/image", 1);
             int_subscriber.subscribe(node, "camera/integer", 1); 
-            //message_filters::TimeSynchronizer<sensor_msgs::Image, imagineer::Number> sync(img_subscriber, int_subscriber, 10);
             sync.registerCallback(boost::bind(&Controller::callback, *this, _1, _2)); // boost::bind() allows to pass arguments to a callback.  
         }
 
