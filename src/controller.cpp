a2af71cf8d47e11da16dd0e8620a40d45bd2005a
Marta Brüning <marta@icloud.com>
2021-05-05 10:48:49 +0200

    working on registerCallback()

--- src/controller.cpp
+++ src/controller.cpp
@@ -45,14 +45,16 @@ class Controller
         std::vector<NumberAndPicture> storage;
         message_filters::Subscriber<sensor_msgs::Image> img_subscriber; 
         message_filters::Subscriber<imagineer::Number> int_subscriber;
-        // message_filters::TimeSynchronizer<sensor_msgs::Image, imagineer::Number> sync;
-
-        Controller() // : sync(img_subscriber, int_subscriber, 10)
+        message_filters::TimeSynchronizer<sensor_msgs::Image, imagineer::Number> sync;
+        
+        Controller() = delete;
+        
+        Controller() : sync(img_subscriber, int_subscriber, 10)
         {
             service_client = node.serviceClient<imagineer::ImageAck>("ImageAck");
             img_subscriber.subscribe(node, "processor/image", 1);
             int_subscriber.subscribe(node, "camera/integer", 1); 
-            message_filters::TimeSynchronizer<sensor_msgs::Image, imagineer::Number> sync(img_subscriber, int_subscriber, 10);
+            //message_filters::TimeSynchronizer<sensor_msgs::Image, imagineer::Number> sync(img_subscriber, int_subscriber, 10);
             sync.registerCallback(boost::bind(&Controller::callback, *this, _1, _2)); // boost::bind() allows to pass arguments to a callback.  
         }
 
