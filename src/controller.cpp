7b6775eeea918b326ae7009d21c9d55b6cd399c1
Marta Brüning <marta@icloud.com>
2021-05-05 11:10:52 +0200

    working on registerCallback()

--- src/controller.cpp
+++ src/controller.cpp
@@ -41,12 +41,12 @@ class Controller
 {
     public:
         
-        Controller() : sync(img_subscriber, int_subscriber, 10)
+        Controller() //: sync(img_subscriber, int_subscriber, 10)
         {
             service_client = node.serviceClient<imagineer::ImageAck>("ImageAck");
             img_subscriber.subscribe(node, "processor/image", 1);
             int_subscriber.subscribe(node, "camera/integer", 1); 
-            sync->registerCallback(boost::bind(&Controller::callback, *this, _1, _2)); // boost::bind() allows to pass arguments to a callback.  
+            sync.registerCallback(boost::bind(&Controller::callback, this, _1, _2)); // boost::bind() allows to pass arguments to a callback.  
         }
 
         /* Sends the image as servide message to the neural network node.
@@ -102,7 +102,7 @@ class Controller
         std::vector<NumberAndPicture> storage;
         message_filters::Subscriber<sensor_msgs::Image> img_subscriber; 
         message_filters::Subscriber<imagineer::Number> int_subscriber;
-        message_filters::TimeSynchronizer<sensor_msgs::Image, imagineer::Number> sync;
+        message_filters::TimeSynchronizer<sensor_msgs::Image, imagineer::Number> sync(img_subscriber, int_subscriber, 10);
 };
 
 /* Entry point for the software program.
