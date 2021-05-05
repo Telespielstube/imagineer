fb858c96d5c226481a59cc996d14815b2934c8eb
Marta Brüning <marta@icloud.com>
2021-05-05 12:27:06 +0200

    trying approximateTimeSync

--- src/controller.cpp
+++ src/controller.cpp
@@ -46,8 +46,7 @@ class Controller
             service_client = node.serviceClient<imagineer::ImageAck>("ImageAck");
             img_subscriber.subscribe(node, "processor/image", 1);
             int_subscriber.subscribe(node, "camera/integer", 1); 
-            sync_.reset(new Sync(MySyncPolicy(10), img_subscriber, int_subscriber));
-            sync.registerCallback(boost::bind(&Controller::callback, this, _1)); // boost::bind() allows to pass arguments to a callback.  
+            sync.registerCallback(boost::bind(&Controller::callback, boost::shared_ptr<imagineer::Number>, _1, _2)); // boost::bind() allows to pass arguments to a callback.  
         }
 
         /* Sends the image as servide message to the neural network node.
