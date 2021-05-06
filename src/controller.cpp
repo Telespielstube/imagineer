26c0fc74a64ea5e8429becc887d2bd761441679c
Marta Brüning <marta@icloud.com>
2021-05-06 14:11:54 +0200

    changed exec in CMakeLists

--- src/controller.cpp
+++ src/controller.cpp
@@ -9,7 +9,33 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "imagineer/Number.h"
 #include "imagineer/ImageAck.h"
-#include "number_and_picture.h"
+
+class NumberAndPicture
+{
+    public:
+        // two contructors, first one is the default constructor, second one expects 2 arguments.
+        NumberAndPicture() {} 
+        NumberAndPicture(const imagineer::Number digit, sensor_msgs::Image image)
+        {
+            num = digit;
+            img = image;
+        }
+        
+        /* operator overloading function which takes argument &other and copies it to a memeber variable.
+        * @other        reference to a parameter to be copied to a member variable .
+        * @return       object reference.
+        */ 
+        NumberAndPicture& operator= (const NumberAndPicture &other)
+        {
+            num = other.num;
+            img = other.img;
+            return *this;
+        }
+
+    private:
+        imagineer::Number num;
+        sensor_msgs::Image img;
+};
 
 class Controller
 {
