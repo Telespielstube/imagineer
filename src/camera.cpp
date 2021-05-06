e5fbee8111ac1a33def3756bc84e770d839246c6
Marta Brüning <marta@icloud.com>
2021-05-06 21:56:59 +0200

    got rid of the last char primitive

--- src/camera.cpp
+++ src/camera.cpp
@@ -28,14 +28,15 @@ std::vector<std::string> get_folder_content(std::string path)
 
 /* Reads the content of the given file and saves content and filename as unorrdered map.
 */
-std::unordered_map<int, sensor_msgs::ImagePtr> read_image(std::vector<std::string> image_files)
+std::unordered_map<imagineer::Number, sensor_msgs::ImagePtr> read_image(std::vector<std::string> image_files)
 {
-    std::unordered_map<std::string, sensor_msgs::ImagePtr> message_to_publish;
-    int filename = 0;
+    std::unordered_map<imagineer::Number, sensor_msgs::ImagePtr> message_to_publish;
+    imagineer::Number filename;
     //fills the unordered map with filename as key and image as value sensor_msgs.  
     for (std::string _file : image_files)
     {
-        filename = (int)_file.substr(16, 17);
+        filename = _file.substr(16, 17).digit;
+
         cv::Mat image = cv::imread(_file, cv::IMREAD_COLOR);
         message_to_publish[filename.digit] = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image)).toImageMsg(); // adds filename as key and cv_bridge Image as value  
     }
@@ -43,7 +44,7 @@ std::unordered_map<int, sensor_msgs::ImagePtr> read_image(std::vector<std::strin
 }
 
 void publish_message(ros::NodeHandle node, image_transport::Publisher img_publisher, ros::Publisher int_publisher, 
-                    std::unordered_map<int, sensor_msgs::ImagePtr> message_list)
+                    std::unordered_map<imagineer::Number, sensor_msgs::ImagePtr> message_list)
 {
     ros::Rate loop(50);
     while (node.ok()) 
@@ -81,6 +82,6 @@ int main(int argc, char** argv)
     
     std::string path(std::string(argv[1]));
     std::vector<std::string> directory_files = get_folder_content(path);
-    std::unordered_map<int, sensor_msgs::ImagePtr> message_list = read_image(directory_files);
+    std::unordered_map<imagineer::Number, sensor_msgs::ImagePtr> message_list = read_image(directory_files);
     publish_message(node, img_publisher, int_publisher, message_list);
 }
