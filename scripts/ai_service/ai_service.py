950203c509da19af9d344b96962b202afabdb536
Marta Brüning <marta@icloud.com>
2021-05-22 22:37:56 +0200

    Correct import numberCruncher in python scrpit

--- scripts/ai_service/ai_service.py
+++ scripts/ai_service/ai_service.py
@@ -3,7 +3,7 @@ import rospy, cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from imagineer.srv import ImageAck, ImageAckResponse
-from ai_service.number_cruncher import NumberCruncher
+from number_cruncher import NumberCruncher
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
