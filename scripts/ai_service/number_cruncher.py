88e80c6555162d86899a28f050b15a5be550ffd1
Marta Brüning <marta@icloud.com>
2021-05-22 22:42:56 +0200

    Correct import numberCruncher in python scrpit

--- scripts/ai_service/number_cruncher.py
+++ scripts/ai_service/number_cruncher.py
@@ -11,7 +11,19 @@ class NumberCruncher:
     
     # Constructor
     # @self    this object every object function has to have self.
-    def __init__(self):
-        super(NumberCruncher, self).__init__()
+        def __init__(self):
+            super(NumberCruncher, self).__init__()
+            self.flatten = nn.Flatten()
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(28*28, 512),
+                nn.ReLU(),
+                nn.Linear(512, 512),
+                nn.ReLU(),
+                nn.Linear(512, 10),
+                nn.ReLU()
+            )
 
-        
+        def forward(self, x):
+            x = self.flatten(x)
+            logits = self.linear_relu_stack(x)
+            return logits
