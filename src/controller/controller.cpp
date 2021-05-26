10b19ba621780309a616c61130e306ea8c98df00
Marta Brüning <marta@icloud.com>
2021-05-26 19:21:42 +0200

    added vector outpu

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -27,7 +27,7 @@ void Controller::add_to_list(int digit, sensor_msgs::Image& image)
     storage.push_back(NumberAndPicture(digit, image));
     for (auto i : storage)
     {
-        std::cout << i << std::endl;
+        std::cout << storarge.at(i) << std::endl;
     }
 }
 
