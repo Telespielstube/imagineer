50205a727b305c8a4b74fa1461b70691607b6dd0
Marta Brüning <marta@icloud.com>
2021-06-07 20:07:40 +0200

    workin on the training function trying to pass Number obj.

--- src/ai_service/ai_service.py
+++ src/ai_service/ai_service.py
@@ -14,13 +14,8 @@ from torchvision.transforms import ToTensor, Lambda, Compose
 # Function is called if the node receives a messages via the subscribed topic.
 # @request    the received image. 
 # @args       arguments passed to callback function.
-def callback(request, model):
+def callback(request):
     response = ImageAckResponse()
-    # model.set_image(request.image)
-    model.training_phase() 
-    print("Model added")
-    
-    
     response.result = 5 ## later the predicted number is passed to response.result
     return response
 
@@ -29,7 +24,9 @@ def callback(request, model):
 def main():
     rospy.init_node('ai_service')
     model = NumberMachine(batch_size=200, epochs=10, learning_rate=0.01, log_interval=10)
-    rospy.Service('image_ack', ImageAck, callback, model)
+    #model.set_image(request.image)
+    model.training_phase() 
+    rospy.Service('image_ack', ImageAck, callback)
     rospy.spin()
 
 # Implies that the script is run standalone and cannot be imported as a module.
