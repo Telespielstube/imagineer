ff20a4a4b32ad991de14de4938da6866a7e86abe
Marta Brüning <marta@icloud.com>
2021-06-08 11:17:02 +0200

    if else for load model

--- src/ai_service/main.py
+++ src/ai_service/main.py
@@ -1,4 +1,5 @@
-import rospy, torch
+import rospy
+import torch
 from sensor_msgs.msg import Image
 from imagineer.srv import ImageAck, ImageAckResponse
 from ai_service.service import Service
@@ -15,20 +16,21 @@ def callback(request):
 def main():
     rospy.init_node('ai_service')
     service = Service()
-
-    if torch.cuda.is_available():
-        torch.device('gpu')
-        print('Using the gpu')
-        service.training_phase_with_cuda()
-        service.save_model()
+    if not service.load_model():
+        # checks if Nvidia cuda support is available. 
+        if torch.cuda.is_available():
+            torch.device('gpu')
+            print('Using the gpu')
+            service.training_phase_with_cuda()
+            service.save_model()
+        else:
+            torch.device('cpu')
+            print('Using the cpu. Get youself a cup of coffee it will take time :-D')
+            service.training_phase_without_cuda()
+            service.save_model()
     else:
-        torch.device('cpu')
-        print('Using the cpu. Get youself a cup of coffee it will take time :-D')
-        service.training_phase_without_cuda()
-        service.save_model()
-
-    rospy.Service('image_ack', ImageAck, callback)
-    rospy.spin()
+        rospy.Service('image_ack', ImageAck, callback)
+        rospy.spin()
 
 # Implies that the script is run standalone and cannot be imported as a module.
 if __name__ == '__main__':
