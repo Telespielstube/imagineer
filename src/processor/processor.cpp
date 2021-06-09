0d1dc2b249fed8092218352331f2f49f2640bbf5
Marta Brüning <marta@icloud.com>
2021-06-09 19:57:47 +0200

    workin on image resizing

--- src/processor/processor.cpp
+++ src/processor/processor.cpp
@@ -3,12 +3,11 @@
 cv::Mat Processor::process_image(cv::Mat& message)
 {
     float scale_down = 0.5;
-    int resized_width = message * scale;
-    int resized_height = message * scale;
+    int resized_width = message * scale_down;
+    int resized_height = message * scale_down;
     cv::Mat resized_message;
     cv::resize(message, resized_message, cv::Size(resized_width, resized_height));
-    cv::Mat processed_image = resized_message;
-    return processed_image;
+    return resized_message;
 }
 
 void Processor::callback(const sensor_msgs::ImageConstPtr& message)
