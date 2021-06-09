1d9113cad204d9009d838dd436f13eefecd4a823
Marta Brüning <marta@icloud.com>
2021-06-09 22:56:55 +0200

    workin on image resizing

--- src/processor/processor.cpp
+++ src/processor/processor.cpp
@@ -9,6 +9,12 @@ cv::Mat Processor::process_image(cv::Mat& message)
     return resized_message;
 }
 
+cv::Mat Processor::color_to_grey(cv::Mat& color_image)
+{
+    cv::Mat greyscale;
+    cv::cvtColor(color_image, greyscale, CV_BGR2GRAY);
+    return greyscale;
+}
 void Processor::callback(const sensor_msgs::ImageConstPtr& message)
 {
     try
@@ -16,7 +22,8 @@ void Processor::callback(const sensor_msgs::ImageConstPtr& message)
         cv::namedWindow("view", cv::WINDOW_AUTOSIZE);
 
         cv::Mat resized_image = process_image(cv_bridge::toCvCopy(message)->image); // Converts the cv_bridge back to a ros image and processes it.
-        cv::imshow("view", resized_image);
+        cv::Mat greyscale =  color_to_grey(resized_image);
+        cv::imshow("view", greyscale);
         cv::waitKey(30); 
         publisher.publish(cv_bridge::CvImage(std_msgs::Header(), "mono8", resized_image).toImageMsg()); 
         ROS_INFO("Image is published.");
