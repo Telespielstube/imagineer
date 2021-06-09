5605fc3ee2d4566c3fbdf7ceb9c667c06a252d46
Marta Brüning <marta@icloud.com>
2021-06-09 11:57:01 +0200

    Changed the save path from realtive to ablsolute

--- src/ai_service/main.py
+++ src/ai_service/main.py
@@ -15,8 +15,8 @@ def callback(request):
 # and checking if Nvidias cuda is available
 def main():
     rospy.init_node('ai_service')
-    service = Service(sys.argv[1])
-    file = pathlib.Path(sys.argv[1])
+    service = Service(sys.argv[0])
+    file = pathlib.Path(sys.argv[0])
     if not file.exists():
         print('Does not exist')
         # checks if Nvidia cuda support is available. 
