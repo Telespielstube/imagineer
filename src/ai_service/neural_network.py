2f179741507ae21a31084705bc105edd2c060d92
Marta Brüning <marta@icloud.com>
2021-06-10 15:35:53 +0200

    added validation mnist

--- src/ai_service/neural_network.py
+++ src/ai_service/neural_network.py
@@ -8,17 +8,17 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()                       
-        self.input_layer = nn.Sequential(nn.Linear(28*28, 512), nn.ReLU()) #first layer has 784 input values, and 512 output values
-        self.hidden_layer1 = nn.Linear(512, 254), nn.ReLU()
-        self.hidden_layer2 = nn.Linear(254, 128), nn.ReLU()#output size is 10, because we expect a number from 0 to 9.
+        self.input_layer = nn.Sequential(nn.Linear(28*28, 512)) #first layer has 784 input values, and 512 output values
+        self.hidden_layer1 = nn.Linear(512, 254)
+        self.hidden_layer2 = nn.Linear(254, 128)#output size is 10, because we expect a number from 0 to 9.
         self.output_layer = nn.Linear(128, 10)
 
     # Forward function
     def forward(self, x):
         x = self.flatten(x)
-        x = self.input_layer(x)
-        x = self.hidden_layer1(x)
-        x = self.hidden_layer2(x)
+        x = F.relu(self.input_layer(x))
+        x = F.relu(self.hidden_layer1(x))
+        x = F.relu(self.hidden_layer2(x))
         x = self.output_layer(x)
         
         return F.log_softmax(x, 1)
