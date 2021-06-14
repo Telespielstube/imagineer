3e3bb8cfbb71adf9e5a1c911d893ce48d88e1829
Marta Brüning <marta@icloud.com>
2021-06-14 16:10:06 +0200

    workin in controller.cpp

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -8,7 +8,6 @@ void Controller::send_image()
     {
         service.request.image = storage.back().get_image(); // image gets passed to the service request image attribute.
         number = storage.back().get_number(); // the corresponding label(number) gets passed to an integer. 
-        service.waitForExistence();
         ROS_INFO("Number sent: %i", number);
     }
     if (service_client.call(service))
