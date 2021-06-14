a4a14a115c9a23fd037a060aba78c1c08e3489dd
Marta Brüning <marta@icloud.com>
2021-06-14 23:23:06 +0200

    added a thirrd layer to network

--- src/ai_service/neural_network.py
+++ src/ai_service/neural_network.py
@@ -11,7 +11,8 @@ class NeuralNetwork(nn.Module):
         self.input_layer = nn.Sequential(nn.Linear(28*28, 512)) #first layer has 784 input values, and 512 output values
         self.hidden_layer1 = nn.Linear(512, 254)
         self.hidden_layer2 = nn.Linear(254, 128)#output size is 10, because we expect a number from 0 to 9.
-        self.output_layer = nn.Linear(128, 10)
+        self.hidden_layer3 = nn.Linear(128, 64)
+        self.output_layer = nn.Linear(64, 10)
 
     # Forward function is the calculation process, it computes output values 
     # from input values. Activation function(Input * weight + bias)
@@ -21,6 +22,7 @@ class NeuralNetwork(nn.Module):
         x = F.relu(self.input_layer(x))
         x = F.relu(self.hidden_layer1(x))
         x = F.relu(self.hidden_layer2(x))
+        x = F.relu(self.hidden_layer3(x))
         x = self.output_layer(x)
         
         return F.log_softmax(x, 1)
