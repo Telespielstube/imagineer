4a6988b1996c6df56c9bb7c89fb914efcaca677f
Marta Brüning <marta@icloud.com>
2021-06-16 21:26:52 +0200

    cleanup and documentin in processor node.

--- src/processor/processor.cpp
+++ src/processor/processor.cpp
@@ -3,21 +3,25 @@
 
 /* Function for image processing like resizing, grayscaling, inverting and adding a threshold 
 *  @message     image object in Opencv format
+*
 *  @return      processed image in Opencv format.
 */
 cv::Mat Processor::process_image(cv::Mat& message)
 {
     cv::Mat resized_message;
-    cv::Mat grayscale_image;//declaring a matrix to store converted image//
-    cv::Mat binary_image;//declaring a matrix to store the binary image
-    cv::Mat inverted_binary_image;
+    cv::Mat grayscale_image;
+    cv::Mat threshold_image;
+    cv::Mat inverted_gray_image;
     cv::resize(message, resized_message, cv::Size(28, 28));
-    cv::cvtColor(resized_message, grayscale_image, cv::COLOR_BGR2GRAY);//Converting BGR to Grayscale image and storing it into 'converted' matrix//
-    cv::threshold(grayscale_image, binary_image, 110, 255, cv::THRESH_BINARY);//converting grayscale image stored in 'converted' matrix into binary image//
-    cv::bitwise_not(binary_image, inverted_binary_image);
+    cv::cvtColor(resized_message, grayscale_image, cv::COLOR_BGR2GRAY);
+    cv::threshold(grayscale_image, threshold_image, 110, 255, cv::THRESH_BINARY);
+    cv::bitwise_not(threshold_image, inverted_gray_image); 
     return inverted_binary_image;
 }
 
+/* The callback function is called whenever a new message is received.
+*  @message    image object in RSO sensor message format.
+*/
 void Processor::callback(const sensor_msgs::ImageConstPtr& message)
 {
     try
@@ -30,6 +34,5 @@ void Processor::callback(const sensor_msgs::ImageConstPtr& message)
     catch (cv_bridge::Exception& e)
     {
         ROS_ERROR("Could not convert received image %s.", message->encoding.c_str());
-       // ROS_ERROR("Could not convert received image %s. Detailed error message: %c", e.what());
     }
 }
