4a31208348013d2412ae3a000acfda2749957931
Marta Brüning <marta@icloud.com>
2021-06-16 23:03:25 +0200

    Changed optim to ASGD and criterion to BCELoss().

--- src/ai_service/ai_service.py
+++ src/ai_service/ai_service.py
@@ -29,7 +29,7 @@ class AiService():
     # Function to train the mnist dataset.
     def training(self):
         criterion = nn.BCELoss() #combines LogSoftmax and NLLLoss in one single class.
-        optimizer = torch.optim.Adam(self.model.parameters(), self.learning_rate)
+        optimizer = torch.optim.ASGD(self.model.parameters(), self.learning_rate)
         for epoch in range(self.epochs):
             running_loss = 0
             # trainig phase
