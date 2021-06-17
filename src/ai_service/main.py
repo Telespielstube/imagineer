6ef95d5247abbaa103e314b66939aaf936bfea14
Marta Brüning <marta@icloud.com>
2021-06-17 21:31:36 +0200

    fixed some bugs in ai_serrvice.py

--- src/ai_service/main.py
+++ src/ai_service/main.py
@@ -8,7 +8,7 @@ from ai_service.ai_service import AiService
 # @request    the received image as sensor message. 
 def callback(request, service):
     response = ImageAckResponse() 
-    response.result = service.validating(request.image)
+    response.result = service.prediction(request.image)
     return response
      
 # Handles all the basics like initializing node, ai_service and the Service server. Checks if a model is already saved 
