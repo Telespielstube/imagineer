e39311c1aa123aa5364d472a432628db711313e7
Marta Brüning <marta@icloud.com>
2021-06-29 11:22:44 +0200

    cleaned up ais_service init

--- src/ai_service/ai_service.py
+++ src/ai_service/ai_service.py
@@ -13,12 +13,11 @@ class AiService():
         self.epochs = 25
         self.learning_rate = 0.01
         self.momentum = 0.9
+        self.transform = transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
         self.training_data = torch.utils.data.DataLoader(datasets.MNIST(root='./data', train=True, download=True, 
-                                transform=transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.1307,), (0.3081,))])), 200, shuffle=True)
+                                self.transform, self.batch_size, shuffle=True)
         self.validation_data = torch.utils.data.DataLoader(datasets.MNIST(root='./data', train=False, download=True, 
-                                transform=transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.1307,), (0.3081,))])), 200, shuffle=True)
+                                self.transform, self.batch_size, shuffle=True)
         self.path = save_path
         self.cv_bridge = CvBridge()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
