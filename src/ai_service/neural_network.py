1ac0d772706ebf49fa0dddc90c5946d9903bf6a5
Marta Brüning <marta@icloud.com>
2021-07-02 13:10:25 +0200

    Added sequential to model

--- src/ai_service/neural_network.py
+++ src/ai_service/neural_network.py
@@ -8,20 +8,16 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()                       
-        self.input_layer = nn.Sequential(nn.Linear(28 * 28, 512)) #first layer has 784 input values (pixels), and 512 output values
-        self.hidden_layer1 = nn.Linear(512, 254)
-        self.hidden_layer2 = nn.Linear(254, 128)
-        self.hidden_layer3 = nn.Linear(128, 64)
-        self.output_layer = nn.Linear(64, 10) # output size is 10, because we expect a number from 0 to 9.
-
+        self.linear_layer = nn.Sequential(nn.Linear(28 * 28, 512), nn.ReLU(), #first layer has 784 input values (pixels), and 512 output values
+        nn.Linear(512, 254), nn.ReLU(), 
+        nn.Linear(254, 128), nn.ReLU(),
+        nn.Linear(128, 64), nn.ReLU(),
+        nn.Linear(64, 10)) # output size is 10, because we expect a number from 0 to 9.
+        
     # Forward function is the calculation process, it computes output values 
     # from input values. Activation function(Input * weight + bias)
     # Activation function -> ReLU 
     def forward(self, x):
         x = self.flatten(x)
-        x = F.relu(self.input_layer(x))
-        x = F.relu(self.hidden_layer1(x))
-        x = F.relu(self.hidden_layer2(x))
-        x = F.relu(self.hidden_layer3(x))
-        x = self.output_layer(x) 
+        x = self.linear_layer(x)
         return x
