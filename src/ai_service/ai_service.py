cf030be45ff2a693c0f3209d6d38aad40c694bc8
Marta Brüning <marta@icloud.com>
2021-07-27 15:27:41 +0200

    added data folder

--- src/ai_service/ai_service.py
+++ src/ai_service/ai_service.py
@@ -5,27 +5,27 @@ from cv_bridge import CvBridge
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor, Compose, Normalize
-from ai_service.neural_network import NeuralNetwork
+#from ai_servic.neural_network import NeuralNetwork
 
-# class NeuralNetwork(nn.Module):
+class NeuralNetwork(nn.Module):
 
-#     # Initializes the Neural Network by setting up the layers.
-#     def __init__(self):
-#         super().__init__()
-#         self.flatten = nn.Flatten()                       
-#         self.linear_layer = nn.Sequential(nn.Linear(28 * 28, 512), nn.ReLU(), #first layer has 784 input values (pixels), and 512 output values
-#         nn.Linear(512, 254), nn.ReLU(), 
-#         nn.Linear(254, 128), nn.ReLU(),
-#         nn.Linear(128, 64), nn.ReLU(),
-#         nn.Linear(64, 10)) # output size is 10, because we expect a number from 0 to 9.
+    # Initializes the Neural Network by setting up the layers.
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()                       
+        self.linear_layer = nn.Sequential(nn.Linear(28 * 28, 512), nn.ReLU(), #first layer has 784 input values (pixels), and 512 output values
+        nn.Linear(512, 254), nn.ReLU(), 
+        nn.Linear(254, 128), nn.ReLU(),
+        nn.Linear(128, 64), nn.ReLU(),
+        nn.Linear(64, 10)) # output size is 10, because we expect a number from 0 to 9.
         
-#     # Forward function is the calculation process, it computes output values 
-#     # from input values. Activation function(Input * weight + bias)
-#     # Activation function -> ReLU 
-#     def forward(self, x):
-#         x = self.flatten(x)
-#         x = self.linear_layer(x)
-#         return x
+    # Forward function is the calculation process, it computes output values 
+    # from input values. Activation function(Input * weight + bias)
+    # Activation function -> ReLU 
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.linear_layer(x)
+        return x
         
 class AiService():
 
