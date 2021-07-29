737510a0571f3a135dffd8ea940b83b5661d78b4
Marta Brüning <marta@icloud.com>
2021-07-29 12:12:49 +0200

    deleted ai_service. import

--- src/ai_service/ai_service.py
+++ src/ai_service/ai_service.py
@@ -6,7 +6,7 @@ from cv_bridge import CvBridge
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor, Compose, Normalize
-from neural_network import NeuralNetwork
+from ai_service.neural_network import NeuralNetwork
 
 # class NeuralNetwork(nn.Module):
 
