57ed52933ed83306fe28d0e145557c459f864628
Marta Brüning <marta@icloud.com>
2021-08-16 12:01:35 +0200

    changed batch size to 200.

--- src/ai_service/ai_service.py
+++ src/ai_service/ai_service.py
@@ -7,31 +7,11 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor, Compose, Normalize
 from ai_service.neural_network import NeuralNetwork
-
-# class NeuralNetwork(nn.Module):
-
-#     # Initializes the Neural Network by setting up the layers.
-#     # Activation function(Input * weight + bias). Activation function -> ReLU 
-#     def __init__(self):
-#         super().__init__()
-#         self.flatten = nn.Flatten()                       
-#         self.linear_layer = nn.Sequential(nn.Linear(28 * 28, 512), nn.ReLU(), #first layer has 784 input values (pixels), and 512 output values
-#         nn.Linear(512, 254), nn.ReLU(), 
-#         nn.Linear(254, 128), nn.ReLU(),
-#         nn.Linear(128, 64), nn.ReLU(),
-#         nn.Linear(64, 10)) # output size is 10, because we expect a number from 0 to 9.
-        
-#     # Forward function is the calculation process, it computes output values 
-#     # from input values. 
-#     def forward(self, x):
-#         x = self.flatten(x)
-#         x = self.linear_layer(x)
-#         return x
         
 class AiService():
 
     def __init__(self):
-        self.batch_size = 32
+        self.batch_size = 200
         self.epochs = 10
         self.learning_rate = 0.001
         self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
